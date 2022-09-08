36aa89bfd2c3eb35ae470c42d7414bea29bff9ca
Allenwhere <zwx971666043@outlook.com>
2022-09-08 11:04:48 -0400

    Swagger(couldn't run

--- employee/src/main/java/com/example/employee/configuration/MyConfiguration.java
+++ employee/src/main/java/com/example/employee/configuration/MyConfiguration.java
@@ -3,12 +3,21 @@ package com.example.employee.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class MyConfiguration {
 
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
+
+//    @Bean
+//    public Docket docket() {
+//        return new Docket(DocumentationType.SWAGGER_2);
+//    }
 }
